78e941ae4baa1a7b7d264ca86019dfd628b39b3f
Irene <35508977+guzman579@users.noreply.github.com>
2019-02-06 17:22:36 -0800

    Create  DoWhileTest.cpp

---
+++ DoWhileTest.cpp
@@ -0,0 +1,18 @@
+// DoWhileTest.cpp
+// do...while repetition statement
+#include <iostream>
+using namespace std;
+
+int main()
+{
+		int counter = 1;
+
+		do
+		{
+			cout<<"  "<<counter;
+			++counter;
+		} while (counter <= 10); // end do...while 
+		cout << endl;
+		system("pause");
+
+} // end class 
